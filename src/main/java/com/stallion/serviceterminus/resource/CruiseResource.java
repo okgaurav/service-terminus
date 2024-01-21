0d16aacd24ea3e2352d4dab56a34c7f5d45c4b1f
Gaurav Verma <gverma0549@gmail.com>
2024-01-22 00:34:12 +0530

    Able to fetch RoutePlan in journey

--- src/main/java/com/stallion/serviceterminus/resource/CruiseResource.java
+++ src/main/java/com/stallion/serviceterminus/resource/CruiseResource.java
@@ -2,9 +2,7 @@ package com.stallion.serviceterminus.resource;
 
 import com.stallion.serviceterminus.model.api.CruiseRequestApiDto;
 import com.stallion.serviceterminus.model.api.TerminalRequestApiDto;
-import com.stallion.serviceterminus.model.entity.config.City;
 import com.stallion.serviceterminus.model.entity.config.Distance;
-import com.stallion.serviceterminus.model.entity.config.Properties;
 import com.stallion.serviceterminus.model.entity.config.TerminusCode;
 import com.stallion.serviceterminus.service.TerminalService;
 import com.stallion.serviceterminus.utils.StallionResponseHandler;
@@ -30,49 +28,48 @@ import static org.springframework.http.HttpStatus.OK;
 public class CruiseResource {
     private final CruiseService cruiseService;
     private final TerminalService terminalService;
-    private final Properties properties;
     private final Distance distance;
     private final TerminusCode terminusCode;
 
-    public CruiseResource(CruiseService cruiseService, TerminalService terminalService, Properties properties, Distance distance, TerminusCode terminusCode) {
+    public CruiseResource(CruiseService cruiseService, TerminalService terminalService, Distance distance, TerminusCode terminusCode) {
         this.cruiseService = cruiseService;
         this.terminalService = terminalService;
-        this.properties = properties;
         this.distance = distance;
         this.terminusCode = terminusCode;
     }
 
     @PostMapping
-    Mono<ResponseEntity<Object>> create(@Valid @RequestBody CruiseRequestApiDto apiDto){
+    Mono<ResponseEntity<Object>> create(@Valid @RequestBody CruiseRequestApiDto apiDto) {
         return cruiseService.save(apiDto).map(CruiseMapper::toApi)
-                .map(o-> StallionResponseHandler.responseBuilder("Created an instance", CREATED,o));
+                .map(o -> StallionResponseHandler.responseBuilder("Created an instance", CREATED, o));
     }
 
     @GetMapping("/{id}")
-    Mono<ResponseEntity<Object>> getById(@PathVariable String id){
+    Mono<ResponseEntity<Object>> getById(@PathVariable String id) {
         return cruiseService.getById(id).map(CruiseMapper::toApi)
-                .map(o-> StallionResponseHandler.responseBuilder("Instance fetched", OK,o));
+                .map(o -> StallionResponseHandler.responseBuilder("Instance fetched", OK, o));
     }
 
     @GetMapping("/{id}/terminals")
-    ResponseEntity<Flux<TerminalRequestApiDto>> getByAccountId(@PathVariable String id){
+    ResponseEntity<Flux<TerminalRequestApiDto>> getByAccountId(@PathVariable String id) {
         return ResponseEntity.ok(terminalService.getTerminalByAccountId(id));
     }
 
     @PatchMapping("/{id}/terminals")
-    Mono<ResponseEntity<Object>> setTerminal(@PathVariable String id, @RequestBody TerminalRequestApiDto terminal){
-        return terminalService.addTerminalByAccountId(terminal,id)
-                .map(o->  StallionResponseHandler.responseBuilder("Instance saved in list", OK,o));
+    Mono<ResponseEntity<Object>> setTerminal(@PathVariable String id, @RequestBody TerminalRequestApiDto terminal) {
+        return terminalService.addTerminalByAccountId(terminal, id)
+                .map(o -> StallionResponseHandler.responseBuilder("Instance saved in list", OK, o));
     }
+
     @DeleteMapping("/{id}/terminals/{terminalId}")
-    Mono<ResponseEntity<Object>> dropTerminal(@PathVariable String id, @PathVariable String terminalId){
-        return terminalService.dropTerminal(id,terminalId)
-                .map(o->  StallionResponseHandler.responseBuilder("Instance removed from list", OK,o));
+    Mono<ResponseEntity<Object>> dropTerminal(@PathVariable String id, @PathVariable String terminalId) {
+        return terminalService.dropTerminal(id, terminalId)
+                .map(o -> StallionResponseHandler.responseBuilder("Instance removed from list", OK, o));
     }
 
     @GetMapping
-    public List<String> properties(){
-        return terminusCode.getActualRoutes("DL","SPV").getRoute().stream()
-                .map(a-> terminusCode.getPlacesCodes().get(a.intValue()).getTitle()).collect(Collectors.toList());
+    public List<String> properties() {
+        return terminusCode.getActualRoutes("DL", "SPV").getRoute().stream()
+                .map(a -> terminusCode.getPlacesCodes().get(a.intValue()).getTitle()).collect(Collectors.toList());
     }
 }
